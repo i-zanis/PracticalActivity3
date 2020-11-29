f50955868df7dc7738024d9f74877a8616037176
i-zanis <ioanlafazanis@gmail.com>
2020-11-29 02:02:44 +0000

    02021129

---
+++ OnlineStore/src/Main.java
@@ -0,0 +1,116 @@
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        boolean run = true;
+        double total_price = 0;
+
+        while (run) {
+            System.out.println("Select products: ");
+            System.out.println("Meat products (Press 1)");
+            System.out.println("Dairy products (Press 2)");
+            System.out.println("Fruit & vegetable products (Press 3)");
+            System.out.println("Bakery products (Press 4)");
+            System.out.println("Drinks (Press 5)");
+            int category = input.nextInt();
+            switch (category){
+                case 1:
+                    System.out.println("700g Pork Loin Steaks 2.99£ (Press 1)");
+                    System.out.println("200g Beef Fillet Steak 6.5£ (Press 2)");
+                    int meats = input.nextInt();
+                    if(meats == 1){
+                        System.out.println("Enter the amount:");
+                        int amount = input.nextInt();
+                        double price = 2.99 * amount;
+                        total_price += price;
+                    }
+                    else if(meats == 2){
+                        System.out.println("Enter the amount:");
+                        int amount = input.nextInt();
+                        double price = 6.5 * amount;
+                        total_price += price;
+                    }
+                    break;
+                case 2:
+                    System.out.println("1L Milk 1.15£ (Press 1)");
+                    System.out.println("350g Cheddar Cheese 2.75£ (Press 2)");
+                    int dairies = input.nextInt();
+                    if(dairies == 1){
+                        System.out.println("Enter the amount:");
+                        int amount = input.nextInt();
+                        double price = 1.15 * amount;
+                        total_price += price;
+                    }
+                    else if(dairies == 2){
+                        System.out.println("Enter the amount:");
+                        int amount = input.nextInt();
+                        double price = 2.75 * amount;
+                        total_price += price;
+                    }
+                    break;
+                case 3:
+                    System.out.println("Bananas Loose 0.13£ (Press 1)");
+                    System.out.println("Whole Cucumber 0.45£ (Press 2)");
+                    int fruits_vegetables = input.nextInt();
+                    if(fruits_vegetables == 1){
+                        System.out.println("Enter the amount:");
+                        int amount = input.nextInt();
+                        double price = 0.13 * amount;
+                        total_price += price;
+                    }
+                    else if(fruits_vegetables == 2){
+                        System.out.println("Enter the amount:");
+                        int amount = input.nextInt();
+                        double price = 0.45 * amount;
+                        total_price += price;
+                    }
+                    break;
+                case 4:
+                    System.out.println("800g Medium Bread 0.99£ (Press 1)");
+                    System.out.println("(20 pack) Chocolate Brownie Bites 1.79£ (Press 2)");
+                    int bakeries = input.nextInt();
+                    if(bakeries == 1){
+                        System.out.println("Enter the amount:");
+                        int amount = input.nextInt();
+                        double price = 0.99 * amount;
+                        total_price += price;
+                    }
+                    else if(bakeries == 2){
+                        System.out.println("Enter the amount:");
+                        int amount = input.nextInt();
+                        double price = 1.79 * amount;
+                        total_price += price;
+                    }
+                    break;
+                case 5:
+                    System.out.println("2L Diet Coke 1.59£ (Press 1)");
+                    System.out.println("950ML Orange Juice 2.49£ (Press 2)");
+                    int drinks = input.nextInt();
+                    if(drinks == 1){
+                        System.out.println("Enter the amount:");
+                        int amount = input.nextInt();
+                        double price = 1.59 * amount;
+                        total_price += price;
+                    }
+                    else if(drinks == 2){
+                        System.out.println("Enter the amount:");
+                        int amount = input.nextInt();
+                        double price = 2.49 * amount;
+                        total_price += price;
+                    }
+                    break;
+            }
+            System.out.println("Do you wish to continue? Yes - Press 1, No - Press 2");
+            int shopping = input.nextInt();
+            if(shopping == 1){
+                continue;
+            }
+            else if(shopping == 2){
+                run = false;
+            }
+        }
+        System.out.println("Total price Is: " + total_price +" £");
+    }
+}
